d1d96ba7908c5d99cea6294a3cb1397e51f06292
Igor Olenchuk <Olenchuk.io@yandex.ru>
2020-07-14 16:24:58 +0300

    Attempt 4

--- homework.py
+++ homework.py
@@ -25,9 +25,11 @@ class Calculator:
     def get_today_stats(self):
         today_stats = 0
         today = dt.date.today()
-        today_stats = sum([record.amount
+        today_stats = sum([
+                           record.amount
                            for record in self.records
-                           if record.date == today])
+                           if record.date == today
+        ])
         return today_stats
 
     def today_remained(self):
@@ -37,11 +39,12 @@ class Calculator:
     def get_week_stats(self):
         week_stats = 0
         today = dt.date.today()
-        week_stats = sum([record.amount
+        week_ago = today - dt.timedelta(days=7)
+        week_stats = sum([
+                          record.amount
                           for record in self.records
-                          if (today - dt.timedelta(days=7))
-                          <= record.date
-                          <= today])
+                          if week_ago <= record.date <= today
+        ])
         return week_stats
 
 
@@ -54,8 +57,7 @@ class CaloriesCalculator(Calculator):
                 'Сегодня можно съесть что-нибудь ещё, но '
                 f'с общей калорийностью не более {amount_remained} кКал'
             )
-        else:
-            return 'Хватит есть!'
+        return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
@@ -65,21 +67,19 @@ class CashCalculator(Calculator):
     RUB_RATE = float(1)
 
     def get_today_cash_remained(self, currency):
+        amount_remained = self.today_remained()
+        if not amount_remained:
+            return 'Денег нет, держись'
         currencies = {
             'eur': ('Euro', self.EURO_RATE),
             'usd': ('USD', self.USD_RATE),
             'rub': ('руб', self.RUB_RATE),
         }
         currency_name, currency_rate = currencies[currency]
-        amount_remained = self.today_remained()
-        if not amount_remained:
-            return 'Денег нет, держись'
-        else:
-            value_currency = (abs(round(amount_remained / currency_rate, 2)))
-            if amount_remained > 0:
-                return f'На сегодня осталось {value_currency} {currency_name}'
-            else:
-                return (
-                    'Денег нет, держись: твой долг - '
-                    f'{value_currency} {currency_name}'
-                )
+        value_currency = abs(round(amount_remained / currency_rate, 2))
+        if amount_remained > 0:
+            return f'На сегодня осталось {value_currency} {currency_name}'
+        return (
+               'Денег нет, держись: твой долг - '
+               f'{value_currency} {currency_name}'
+        )
